2a7b88bec74377545871bad2d1079187576e81fa
johnngugi <johnngugi777@gmail.com>
2016-07-21 04:36:16 +0300

    Add form validation

--- js/script.js
+++ js/script.js
@@ -1,5 +1,14 @@
+var x = /^[-a-z0-9~!$%^&*_=+}{\'?]+(\.[-a-z0-9~!$%^&*_=+}{\'?]+)*@([a-z0-9_][-a-z0-9_]*(\.[-a-z0-9_]+)*\.(aero|arpa|biz|com|coop|edu|gov|info|int|mil|museum|name|net|org|pro|travel|mobi|[a-z][a-z])|([0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}))(:[0-9]{1,5})?$/i;
 function myFunction() {
        var input = document.getElementById('input');
        var div = document.getElementById('output');
        div.innerHTML = input.value;
+
      }
+
+function myEmail() {
+    var input = document.getElementById('email');
+    if (input != x) {
+      input.value = "Not a valid email address";
+      }
+}
